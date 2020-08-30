63c2c3d7961848bb55c305b45e7a9fc7f76481cb
Anderson <anderson@activebilling.com.au>
2020-08-31 08:34:50 +1000

    task: Implement Interactive mode for CLI
    Implement interactive mode so user can type commands on the fly

---
+++ main.rb
@@ -0,0 +1,25 @@
+require 'pry'
+require_relative 'dsl'
+require_relative 'board'
+
+board = Board.new
+
+instruction_filpath = ARGV[0]
+
+if instruction_filpath
+  commands = DSL.parse_file instruction_filpath
+  commands.each { |command| board.process_command command }
+else
+  puts "[Interactive mode]"
+  puts "Enter instructions:"
+  puts "(press crtl-d to exit)"
+
+  while true
+    input  = gets
+
+    return unless input
+
+    command = DSL.parse_string input.chomp
+    board.process_command command if !command.nil?
+  end
+end
